3cfa47202152ffc32e5051908ba501a061cafd61
mike <mikepro@mikepro.com>
2013-09-01 23:56:35 +0100

    Example of a custom directive

--- js/app.js
+++ js/app.js
@@ -12,11 +12,14 @@ app.config(function($routeProvider){
     .otherwise({redirectTo:'/login'});
 });
 
-app.controller('HomeController',function($scope){});
-app.controller('LoginController',function($scope, $location){
+app.controller('HomeController',function($scope){
+    $scope.jumboMessage = "Please select one of the images below to find out what the name of the landmark is.";
+});
+app.controller('LoginController',function($scope, $location,$rootScope){
     $scope.login = function(){
         if($scope.username=="mike")
         {
+            $rootScope.username = $scope.username;
             $location.path('/home');
         }
         else
@@ -24,5 +27,23 @@ app.controller('LoginController',function($scope, $location){
             $scope.errorMessage ={msg:'Could not find user'};
         }
     }
+});
+
+
+app.directive('displayLandmarkName', function(){
+    return {
+        restrict: "A",
+        link: function(scope, element, attributes){
+            var orgMessage = scope.jumboMessage;
+            element.bind("mouseover",function(){
+                scope.jumboMessage = attributes.landmark;
+                scope.$apply();
+            });
+            element.bind("mouseout",function(){
+                scope.jumboMessage = orgMessage;
+                scope.$apply();
+            });
+        }
+    };
 });
 
